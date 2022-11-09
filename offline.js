48350c8a11f09b73da42cf537e40c15bb2e32868
saitrzhaka <117861741+saitrzhaka@users.noreply.github.com>
2022-11-10 01:07:42 +0500

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1668024381,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/rock-sheet0.png",
+		"images/paper-sheet0.png",
+		"images/scissors-sheet0.png",
+		"images/listener-sheet0.png",
+		"media/paper.ogg",
+		"media/rock.ogg",
+		"media/scissors.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
